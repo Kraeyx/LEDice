342b362e8d8c26438603c57a34c55f7d67f61ebc
Kraeyx <jakob.deutschendorf@gmail.com>
2023-10-18 12:19:55 +0200

    Formatierungsoptimierung

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <Arduino.h>
 
-using namespace std;
-
 const int LED_COUNT = 7;
 const int LED_PINS[LED_COUNT] = {1, 2, 3, 4, 5, 6, 7};
 const int BUTTON_PIN = 12;
-// put function declarations here:
-int myFunction(int, int);
+
 /**
  * Initialisiert die Pins und den Initialzustand
 */
@@ -20,44 +17,44 @@ void setup() {
  * Zeigt über die LEDs eine Nummer an
 */
 void showNumber(int num) {
-switch(num) {
-  case 1: 
-    digitalWrite(LED_PINS[5], HIGH);
-    break;
-  case 2: 
-    digitalWrite(LED_PINS[0], HIGH);
-    digitalWrite(LED_PINS[6], HIGH);
-    break;
-  case 3:
-    digitalWrite(LED_PINS[0], HIGH);
-    digitalWrite(LED_PINS[3], HIGH);
-    digitalWrite(LED_PINS[6], HIGH);
-    break;
-  case 4: 
-    digitalWrite(LED_PINS[0], HIGH);
-    digitalWrite(LED_PINS[2], HIGH);
-    digitalWrite(LED_PINS[4], HIGH);
-    digitalWrite(LED_PINS[6], HIGH);
-    break;
-  case 5: 
-    digitalWrite(LED_PINS[0], HIGH);
-    digitalWrite(LED_PINS[2], HIGH);
-    digitalWrite(LED_PINS[3], HIGH);
-    digitalWrite(LED_PINS[4], HIGH);
-    digitalWrite(LED_PINS[6], HIGH);
-    break;
-  case 6:
-    digitalWrite(LED_PINS[0], HIGH);
-    digitalWrite(LED_PINS[1], HIGH);
-    digitalWrite(LED_PINS[2], HIGH);
-    digitalWrite(LED_PINS[4], HIGH);
-    digitalWrite(LED_PINS[5], HIGH);
-    digitalWrite(LED_PINS[6], HIGH);
-    break;
-  default: 
-    Serial.print("Fehler! Nummer ungültig");
-    break;
-  }
+  switch(num) {
+    case 1: 
+      digitalWrite(LED_PINS[5], HIGH);
+      break;
+    case 2: 
+      digitalWrite(LED_PINS[0], HIGH);
+      digitalWrite(LED_PINS[6], HIGH);
+      break;
+    case 3:
+      digitalWrite(LED_PINS[0], HIGH);
+      digitalWrite(LED_PINS[3], HIGH);
+      digitalWrite(LED_PINS[6], HIGH);
+      break;
+    case 4: 
+      digitalWrite(LED_PINS[0], HIGH);
+      digitalWrite(LED_PINS[2], HIGH);
+      digitalWrite(LED_PINS[4], HIGH);
+      digitalWrite(LED_PINS[6], HIGH);
+      break;
+    case 5: 
+      digitalWrite(LED_PINS[0], HIGH);
+      digitalWrite(LED_PINS[2], HIGH);
+      digitalWrite(LED_PINS[3], HIGH);
+      digitalWrite(LED_PINS[4], HIGH);
+      digitalWrite(LED_PINS[6], HIGH);
+      break;
+    case 6:
+      digitalWrite(LED_PINS[0], HIGH);
+      digitalWrite(LED_PINS[1], HIGH);
+      digitalWrite(LED_PINS[2], HIGH);
+      digitalWrite(LED_PINS[4], HIGH);
+      digitalWrite(LED_PINS[5], HIGH);
+      digitalWrite(LED_PINS[6], HIGH);
+      break;
+    default: 
+      Serial.print("Fehler! Nummer ungültig");
+      break;
+    }
 }
 /**
  * Verdunkelt alle LEDs
@@ -72,9 +69,9 @@ void clearLED() {
  * Testet auf einen Knopfdruck und führt, wenn nötig, den Würfelvorgang durch
 */
 void loop() {
-if(digitalRead(BUTTON_PIN)) {
-  clearLED();
-  int result = 1 + rand() % 5;
-  showNumber(result);
-}
+  if(digitalRead(BUTTON_PIN)) {
+    clearLED();
+    int result = 1 + rand() % 5;
+    showNumber(result);
+  }
 }
